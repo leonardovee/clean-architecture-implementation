f9b7ed6ca555b51fc8dd754937a2201eb457a903
leonardovee <leoveeph@gmail.com>
2021-02-07 21:10:33 -0300

    test: ensure LoadUserByEmailRepository throws if no user model is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -47,4 +47,10 @@ describe('LoadUserByEmail Repository', () => {
       password: fakeUser.ops[0].password
     })
   })
+
+  test('Should throw if no user model is provided', () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })
