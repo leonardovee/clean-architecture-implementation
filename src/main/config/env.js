9704bdeb03eab45a8fe68536b3e053db9ff849fa
leonardovee <leoveeph@gmail.com>
2021-02-09 00:04:24 -0300

    test: ensure LoginRoutes return 200 when valid credentials are provided

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-architecture-implementation',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://leonardovee:yGGxSd51Eg3Owu5p@cluster0.5umt5.mongodb.net/clean-architecture-implementation?retryWrites=true&w=majority',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
   port: process.env.PORT || 5050
 }
